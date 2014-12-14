1a0af478dc7d9f6fe3140b31f879f90f5f858a0e
gab14 <gasolz14@gmail.com>
2014-12-13 22:36:09 -0500

    Paso 1

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<title>Queen Experience Perú Tour 2015</title>
+<link rel="stylesheet" href="normalize.css">
+<link rel="stylesheet" href="style.css">
+<script src="jquery-1.11.1.min.js"></script>
+</head>
+<body>
+	
+	<div class="pagewrapper">
+		<div class="header clearfix">
+			<div class="logo">
+				<img src="img/logo.jpg" alt="">
+				<span class="txt">Sigue 3 simples pasos y disfruta Queen Experience</span>
+			</div>
+			<div class="queen"><img src="img/queen.jpg" alt=""></div>
+		</div>
+
+		<div class="stepwrapper">
+			<div class="centerstep clearfix">
+				<!-- STEP 1 -->
+				<div class="step">
+					<img src="img/uno.jpg" alt="">
+					<span class="txt">INGRESA TUS DATOS AL FORMULARIO</span>
+				</div>
+				
+				<!-- ARROW -->
+				<div class="arrow"><img src="img/arrow.jpg" alt=""></div>
+
+				<!-- STEP 2 -->
+				<div class="step">
+					<img src="img/dos.jpg" alt="">
+					<span class="txt">DEPOSITA EL MONTO DEL<br>VALOR EN...</span>
+				</div>
+
+				<!-- ARROW -->
+				<div class="arrow"><img src="img/arrow.jpg" alt=""></div>			
+
+				<!-- STEP 3 -->
+				<div class="step">
+					<img src="img/tres.jpg" alt="">
+					<span class="txt"><span class="amarillo">DELIVERY</span> EN TU EMPRESA<br>DEL 11 DE ENERO AL 16 DE ENERO PODRÁS RECOGER TUS ENTRADAS MOSTRANDO EL VOUCHER AL PERSONAL AUTORIZADO DE QUEEN EXPERIENCE PRODUCCIONES</span>
+				</div>
+			</div>
+		</div>
+
+		<div class="bannerwrapper">
+			<div class="banner">
+				<img src="img/banner.jpg" alt="">
+			</div>
+		</div>
+
+		<div class="content clearfix">
+			<div class="zoneswrapper">
+				<div class="num">
+					<img src="img/uno.jpg" alt="">
+				</div>
+
+				<div class="zones">
+					<img src="img/zonas.jpg" alt="">
+				</div>
+			</div>
+
+			<div class="formulario">
+				<div class="title">FORMULARIO</div>
+
+				<form action="">
+					<div class="inputbox clearfix">
+						<label>NOMBRE:</label>
+						<div class="input"><input type="text" /></div>
+					</div>
+
+					<div class="inputbox clearfix">
+						<label>APELLIDO:</label>
+						<div class="input"><input type="text" /></div>
+					</div>
+
+					<div class="inputbox clearfix">
+						<label>EMAIL:</label>
+						<div class="input"><input type="text" /></div>
+					</div>
+
+					<div class="inputbox clearfix">
+						<label>EMPRESA:</label>
+						<div class="input"><input type="text" /></div>
+					</div>
+
+					<div class="inputbox clearfix">
+						<label>ZONA:</label>
+						<div class="input">
+							<select name="zona" id="zona">
+								<option value="ZONA GENERAL: S/.91">ZONA GENERAL: S/.91</option>
+								<option value="ZONA VIP: S/.149">ZONA VIP: S/.149</option>
+								<option value="ZONA QUEEN: S/.212">ZONA QUEEN: S/.212</option>
+							</select>
+						</div>
+					</div>
+
+					<div class="inputbox clearfix">
+						<label>Nº DE ENT.:</label>
+						<div class="input"><input type="text" /></div>
+					</div>
+
+					<div class="inputbox clearfix">
+						<label>LOCAL:</label>
+						<div class="input">
+							<select name="local" id="local">
+								<option value="OPHERA">OPHERA</option>
+								<option value="SCENCIA">SCENCIA</option>
+							</select>
+						</div>
+					</div>
+
+					<div class="btn_enviar">
+						<a href="#"><img src="img/ingresar.jpg" alt="" /></a>
+					</div>
+				</form>
+			</div>
+		</div>
+
+		<div class="condiciones">
+			<div class="zona_queen">
+				<span>*ZONA QUEEN : PARA PODER SEPARAR EL BOX DEBEN SER 10 PERSONAS</span>
+				<span>
+					INLCUYE: 1 botella Johnnie Walker Black Label+ 4 energizantes + 1 cajetilla de Lucky Strike+ 1 botella Smirnoff
+				</span>
+
+				<span class="amarillo">
+					*Las entradas serán entregadas por el equipo de producción de QUEEN EXPERIENCE entre el 11 al 16 de enero
+				</span>
+
+				<span class="amarillo">
+					*Para mayor información comunicarse a queenexperienceperu@gmail.com telf: 9941688405 / 957668050
+				</span>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
